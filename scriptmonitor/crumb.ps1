862b69368340e5b281032ff40054afb2bd62b034
John Swails <john.swails@gmail.com>
2018-05-26 07:33:36 -0400

    Update crumb.ps1

--- scriptmonitor/crumb.ps1
+++ scriptmonitor/crumb.ps1
@@ -3,11 +3,11 @@
  $smdir =  "C:\Packages\scriptmonitor"
  $datetime = Get-Date -Format "yyyyMMdd";
  $datetimefull = Get-Date -Format "yyyyMMddHHmmss";
- $configroot = "\\admin3\scriptmonitor"
+ $configroot = "\\server\scriptmonitor"
  $datetime | Out-File -append $smdir\bug.log
  $machinename = $env:COMPUTERNAME
  $err = ""
- $wipserver = "\\10.30.164.71\data-in"
+ $wipserver = "\\server\data-in"
  $sccmexeclog = "c:\windows\ccm\logs\execmgr.log"
  $os = (Get-CimInstance Win32_OperatingSystem).version
  $proc = $env:PROCESSOR_ARCHITECTURE
