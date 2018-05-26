b61c2b2d45573c1ed8702b8b0a98a0744c4944c5
John Swails <john.swails@gmail.com>
2018-05-26 07:44:21 -0400

    Update dns-script.ps1

--- random-old/dns-script.ps1
+++ random-old/dns-script.ps1
@@ -12,11 +12,11 @@ if  ($FileExists -eq $false) {
     Write-Host 'Credential file not found. Enter your password:' -ForegroundColor Red
     Read-Host -AsSecureString | ConvertFrom-SecureString | Out-File $CredsFile
     $password = get-content $CredsFile | convertto-securestring
-    $Cred = new-object -typename System.Management.Automation.PSCredential -argumentlist SAI\$AdminName,$password}
+    $Cred = new-object -typename System.Management.Automation.PSCredential -argumentlist domain\$AdminName,$password}
 else
     {Write-Host 'Using your stored credential file' -ForegroundColor Green
     $password = get-content $CredsFile | convertto-securestring
-    $Cred = new-object -typename System.Management.Automation.PSCredential -argumentlist SAI\$AdminName,$password}
+    $Cred = new-object -typename System.Management.Automation.PSCredential -argumentlist domain\$AdminName,$password}
 
 ############################
 #      Menu                #
