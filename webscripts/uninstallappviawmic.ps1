4f64d9034f85cf3b1734b4664b332af7964e80fa
John Swails <john.swails@gmail.com>
2018-05-26 07:27:57 -0400

    Delete uninstallappviawmic.ps1

--- webscripts/uninstallappviawmic.ps1
+++
@@ -1,8 +0,0 @@
-﻿Param([string]$mach,[string]$app)
-
-#wmic /failfast:on /node:$mach product get name
-#wmic /failfast:on /node:$mach product where name="PowerGREP" call uninstall /nointeractive
-$mach = "dg8xbpm1"
-$app = "7-zip"
-wmic /failfast:on /node:$mach product where "name like '%$app%'" call uninstall /nointeractive | out-file c:\temp\uninstall.txt
-
