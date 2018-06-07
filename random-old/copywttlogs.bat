18fd220455bbfa343c04a4204a998a870ea10e1b
John Swails <john.swails@gmail.com>
2018-06-07 19:54:36 -0400

    Delete copywttlogs.bat

--- random-old/copywttlogs.bat
+++
@@ -1,7 +0,0 @@
-
-mkdir "\\wwtdfs01\share\v-joswai\wtt-issues\may222011\%1"
-xcopy "\\%1\c$\Program Files (x86)\WTTMobile\Client\DevicePoint_output.txt" "\\wwtdfs01\share\v-joswai\wtt-issues\may222011\%1"
-
-xcopy "\\%1\c$\Program Files (x86)\WTTMobile\Client\WTTClient_output.txt" "\\wwtdfs01\share\v-joswai\wtt-issues\may222011\%1"
-
-xcopy "\\%1\c$\Program Files (x86)\WTTMobile\Client\WTTMessaging.log" "\\wwtdfs01\share\v-joswai\wtt-issues\may222011\%1"
