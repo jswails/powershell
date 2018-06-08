7f9513e691e390e1824d904fb6d93b8895630963
John Swails <john.swails@gmail.com>
2018-06-08 06:04:01 -0400

    Delete web-addlocaladmin.ps1

--- random-old2/web-addlocaladmin.ps1
+++
@@ -1,9 +0,0 @@
-﻿Param([string]$strComputer,[string]$username)
-
-$domain = "SAI"
-$computer = [ADSI]("WinNT://" + $strComputer + ",computer")
-$computer.name | out-file -append e:\wamp\www\iam\admin.csv
-$Group = $computer.psbase.children.find("administrators")
-$group.name | out-file -append e:\wamp\www\iam\admin.csv
-
-$Group.Add("Winnt://" + $domain + "/" + $username)
