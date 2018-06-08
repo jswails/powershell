d027845bf87a2335d6968c05a69231db1e1680c9
John Swails <john.swails@gmail.com>
2018-06-08 05:36:04 -0400

    Delete Untitled3.ps1

--- random-old2/Untitled3.ps1
+++
@@ -1,8 +0,0 @@
-﻿$getcc = get-service -displayname "AppSense Client Communications Agent"  | Select-Object status
-$getccs = $getcc.status
-if ($getccs -eq "stopped" ){
-write-host "cca stopped"
-}
-if ($getccs -eq "running" ){
-write-host "cca running"
-}
