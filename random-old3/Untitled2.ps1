0da37def56b46d029a1bd6dfa20d6fee9220e0b9
John Swails <john.swails@gmail.com>
2018-06-08 18:37:19 -0400

    Delete Untitled2.ps1

--- random-old3/Untitled2.ps1
+++
@@ -1,9 +0,0 @@
-﻿
-  $messageparameters = @{
-  Subject = "Sync failed after 5 days for $env:COMPUTERNAME.$env:USERNAME"
-  body = "Sync failed after 5 days for $env:COMPUTERNAME.$env:USERNAME"
-  from = "john.swails@stateauto.com"
-  to = "john.swails@stateauto.com"
-  smtpserver = "OutlookDC1.corp.stateauto.com"
-  }
-  send-mailmessage @messageparameters -bodyashtml
