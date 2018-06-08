131e0c89a8ad6b835f05bc9509b55d9261bcc9e6
John Swails <john.swails@gmail.com>
2018-06-08 05:37:44 -0400

    Update app-portal-requests.ps1

--- random-old2/app-portal-requests.ps1
+++ random-old2/app-portal-requests.ps1
@@ -1,10 +1,10 @@
-﻿$siteserver = "sadc1cm12p1.corp.stateauto.com" 
+﻿$siteserver = "" 
 $provider = gwmi -ComputerName $siteserver -namespace "root\sms" -query "SELECT * FROM SMS_ProviderLocation"; 
 $prov_machine = $provider.Machine; 
 $sitecode = $provider.SiteCode; 
 $namespace = "root\sms\site_"+$sitecode;
 #$wmiquery = "SELECT * FROM SMS_Application";
-$wmiquery = "SELECT * from SMS_R_User where username = 'swa4444' "
+$wmiquery = "SELECT * from SMS_R_User where username = '' "
 
 #lists all application apps
 $adminapp = gwmi -ComputerName $prov_machine -namespace $namespace -query $wmiquery; 
