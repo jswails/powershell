eb3f53f2cd3aa5a80cf3b6eaa6e6360a20121d55
John Swails <john.swails@gmail.com>
2018-06-08 05:28:12 -0400

    Update logincapture.ps1

--- random-old/logincapture.ps1
+++ random-old/logincapture.ps1
@@ -1,7 +1,8 @@
 $details1 = (get-eventlog application | where-object {$_.EventID -eq "1085"})[0].TimeGenerated
 
 $y = get-date
-$compname = $env:COMPUTERNAME
+
+$compname = $env:COMPUTERNAME
 $username = $env:USERNAME
 $osbuild = $win32os.buildnumber
 $imgver = $env:imgver
@@ -17,8 +18,8 @@ $runtime = $y - $details1
 $disp = ($compname,$proc,$model,$osbuild,$imgver,$logsrv,$username, $curtime, $runtime ) -join "^"
 $disp | Out-File -Append c:\temp\lastuser.txt
 
-$SQLServer = "savfssccmt1" #use Server\Instance for named SQL instances!
- $SQLDBName = "logontime"
+$SQLServer = "" #use Server\Instance for named SQL instances!
+ $SQLDBName = ""
 $SqlQuery = "INSERT INTO dbo.logindata1 (computername,processor,Model,OSbuild,IMGVER,LogonServer,username,runtime,totaltime) VALUES ('$compname','$proc','$model','$osbuild','$imgver','$logsrv','$username','$curtime','$runtime')"
 $SqlConnection = New-Object System.Data.SqlClient.SqlConnection
 $SqlConnection.ConnectionString = "Server = $SQLServer; Database = $SQLDBName; Integrated Security = True"
