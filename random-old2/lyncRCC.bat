5dfc8cc1e5a866e9944d493aa52ee8131c634995
John Swails <john.swails@gmail.com>
2018-06-08 05:43:15 -0400

    Delete lyncRCC.bat

--- random-old2/lyncRCC.bat
+++
@@ -1,9 +0,0 @@
-
-
-reg add HKCU\Software\Policies\Microsoft\Communicator /v TelephonyMode /t REG_DWORD /d 2 /f
-reg add HKCU\Software\Policies\Microsoft\Communicator /v DisablePC2PCAUDIO /t REG_DWORD /d 0 /f
-reg add HKCU\Software\Policies\Microsoft\Communicator /v EnablePhoneControl /t REG_DWORD /d 1 /f
-
-reg add HKLM\Software\Policies\Microsoft\Communicator /v TelephonyMode /t REG_DWORD /d 2 /f
-reg add HKLM\Software\Policies\Microsoft\Communicator /v DisablePC2PCAUDIO /t REG_DWORD /d 0 /f
-reg add HKLM\Software\Policies\Microsoft\Communicator /v EnablePhoneControl /t REG_DWORD /d 1 /f
