b118c2026c0025a0dcf38aaa00f8e54c41a6bd16
John Swails <john.swails@gmail.com>
2018-06-08 05:32:53 -0400

    Delete watsoninstall.bat

--- random-old/watsoninstall.bat
+++
@@ -1,4 +0,0 @@
-
-
-xcopy /y /s C:\QFE\LKG\EPX_1_22_2011_Release\PODs\214655_214656_219050_229436_244326\WatsonUploader\* \\%%i\c$\
-psexec \\%%i c:\WUUninstallInstall.cmd c:\WatsonUploaderServiceInstaller.msi
