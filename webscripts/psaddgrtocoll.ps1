f7a7bdd714f4e4b2c71185a50f615d061772e24c
John Swails <john.swails@gmail.com>
2018-06-24 21:50:41 -0400

    Update psaddgrtocoll.ps1

--- webscripts/psaddgrtocoll.ps1
+++ webscripts/psaddgrtocoll.ps1
@@ -5,6 +5,6 @@ $users|  out-file -append e:\wamp\logs\uservars.log
 $collid | out-file -append e:\wamp\logs\uservars.log
 import-module "e:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\configurationmanager.psd1"
  
-cd C05:
+cd psdrivename:
 
 Add-CMUserCollectionQueryMembershipRule -CollectionID $collid -QueryExpression "select SMS_R_USER.ResourceID,SMS_R_USER.ResourceType,SMS_R_USER.Name,SMS_R_USER.UniqueUserName,SMS_R_USER.WindowsNTDomain from SMS_R_User where SMS_R_User.UserGroupName = '$users'" -RuleName "$users"
