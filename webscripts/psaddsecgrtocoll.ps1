16ddd814776442ec7574792832d3f96a285474cc
John Swails <john.swails@gmail.com>
2018-06-24 21:47:36 -0400

    Update psaddsecgrtocoll.ps1

--- webscripts/psaddsecgrtocoll.ps1
+++ webscripts/psaddsecgrtocoll.ps1
@@ -5,6 +5,6 @@ $users|  out-file -append e:\wamp\logs\uservars.log
 $collid | out-file -append e:\wamp\logs\uservars.log
 import-module "e:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\configurationmanager.psd1"
  
-cd C05:
+cd psdrivename:
 
 Add-CMUserCollectionQueryMembershipRule -CollectionID $collid -QueryExpression "select *  from  SMS_R_UserGroup where SMS_R_UserGroup.Name = '$users'" -RuleName "$users"
